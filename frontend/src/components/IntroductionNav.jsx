8deff575873025b92a5a79bc89811301489fd40f
argiiahm <argiigmg@gmail.com>
2025-11-14 19:19:15 +0700

    remove login

--- frontend/src/components/IntroductionNav.jsx
+++ frontend/src/components/IntroductionNav.jsx
@@ -5,7 +5,7 @@ function IntroductionNav() {
     <div className="bg-purple-500 hidden lg:flex md:flex justify-center">
       <span className="text-white flex items-center gap-4">
         <span className="font-bold">Introduction</span> -Lets Connect!
-        <Link to="/login" className="bg-green-500 px-6 py-2">Masuk</Link>
+        {/* <Link to="/login" className="bg-green-500 px-6 py-2">Masuk</Link> */}
       </span>
     </div>
   );
