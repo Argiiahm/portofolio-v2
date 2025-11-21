aa672420024bb7927ccb10d2e473afaeb31c1735
argiiahm <argiigmg@gmail.com>
2025-11-21 20:23:37 +0700

    railway

--- backend/config/Databases.js
+++ backend/config/Databases.js
@@ -4,21 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS, {
+
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    logging: false,
-  }
-);
-
-export default db;
+    dialect: process.env.DB_DIALECT,
+});
+    
+export default db
