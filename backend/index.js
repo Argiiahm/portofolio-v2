5f84d720819e9c1954d0eb14b4f60c67cfcf9343
argiiahm <argiigmg@gmail.com>
2025-11-22 14:36:23 +0700

    index

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
 import express from "express";
 import fileUpload from "express-fileupload";
-import cors from "cors"
-import ProjectsRoute from "./routes/ProjectsRoute.js"
+import cors from "cors";
+import ProjectsRoute from "./routes/ProjectsRoute.js";
 
-const app = new express();
+const app = express();
 
 app.use(cors());
-app.use(express.json())
-app.use(fileUpload())
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(ProjectsRoute);
 
-app.use(express.static('public'))
-app.use(ProjectsRoute)
-
+app.get("/", (req, res) => {
+  res.send("Backend online...");
+});
 
 const PORT = process.env.PORT || 4000;
-app.listen(4000, () => {
-    console.log(`Server Running On Port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`);
+});
