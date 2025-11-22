fb2ba3020c9904afcf64e3e9bc128724c954d7da
argiiahm <argiigmg@gmail.com>
2025-11-22 15:15:10 +0700

    medsos

--- frontend/src/components/MedsosCard.jsx
+++ frontend/src/components/MedsosCard.jsx
@@ -1,16 +1,22 @@
-import { Github, Linkedin, Instagram } from 'lucide-react'
+import { Github, Linkedin, Instagram } from "lucide-react";
 
 function MedsosCard() {
   return (
     <div className="my-8 text-white font-semibold text-2xl flex gap-4">
       <div className="border p-3 border-[#15273d] shadow rounded-md cursor-pointer hover:bg-gray-800">
-        <Github />
+        <a href="https://github.com/Argiiahm">
+          <Github />
+        </a>
       </div>
       <div className="border p-3 border-[#15273d] shadow rounded-md cursor-pointer hover:bg-gray-800">
-        <Linkedin />
+        <a href="https://www.linkedin.com/in/argi-ahmes-halepiyandra-6b4403329">
+          <Linkedin />
+        </a>
       </div>
       <div className="border p-3 border-[#15273d] shadow rounded-md cursor-pointer hover:bg-gray-800">
-        <Instagram />
+        <a href="https://www.instagram.com/argiiahm">
+          <Instagram />
+        </a>
       </div>
     </div>
   );
